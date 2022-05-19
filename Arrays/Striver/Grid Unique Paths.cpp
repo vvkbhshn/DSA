79c193b254fd8af3f44ba86f9f61ec75da1096a5
Vivek <vivekbhushan7739@gmail.com>
2022-05-19 17:36:46 +0530

    Striver-Arrays part-III

--- Arrays/Striver/Grid Unique Paths.cpp
+++ Arrays/Striver/Grid Unique Paths.cpp
@@ -18,12 +18,12 @@ public:
         }
         return res;
         
-        // Method-2: DP solution,  O(m*n) time
-        // int dp[101][101]={1};
+        // Method-2: DP solution,  O(m*n) time and O(m*n) space
+        // This can be further optimized to O(n) space since we only need values from previous row
+        // vector<vector<int>> dp(m,vector<int>(n,1));
         // for(int i=0;i<m;i++){
         //     for(int j=0;j<n;j++){
-        //         if(j>0) dp[i][j]+=dp[i][j-1];
-        //         if(i>0) dp[i][j]+=dp[i-1][j];
+        //         if(i && j) dp[i][j]=dp[i][j-1]+dp[i-1][j];
         //     }
         // }
         // return dp[m-1][n-1];
